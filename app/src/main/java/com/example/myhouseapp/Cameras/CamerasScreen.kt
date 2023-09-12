8950ce3eea7715f203e76d0f0c09bc65b9d86c9a
katerina <pochenyuk.katya@mail.ru>
2023-09-12 11:26:43 +0300

    Setting star icon was changed

--- app/src/main/java/com/example/myhouseapp/Cameras/CamerasScreen.kt
+++ app/src/main/java/com/example/myhouseapp/Cameras/CamerasScreen.kt
@@ -17,10 +17,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -76,7 +72,7 @@ class CamerasScreen {
 
     @Composable
     fun CardCamera(model: ItemDataBase) {
-        var isStarVisible by remember { mutableStateOf(false) }
+        var isStarVisible = model.favorites!!
 
         val favIconButton = SwipeAction(
             onSwipe = {
